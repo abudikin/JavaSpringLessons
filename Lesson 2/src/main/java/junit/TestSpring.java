85b0fb57ba8da1e6db3a50ea31c39abd4aab9328
abudikin <54790361+abudikin@users.noreply.github.com>
2023-12-03 18:59:11 +0900

    first 7 lessons

---
+++ Lesson 2/src/main/java/junit/TestSpring.java
@@ -0,0 +1,16 @@
+package junit;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testbean = context.getBean("testBean",TestBean.class);
+        System.out.println(testbean.getName());
+
+        context.close();
+    }
+}
---
+++ Lesson 4/src/main/java/junit/ClassicalMusic.java
@@ -0,0 +1,9 @@
+package junit;
+
+public class ClassicalMusic implements Music{
+
+    @Override
+    public String getSong() {
+        return "SONG classic";
+    }
+}
